cfadc8a35dd61a7a84ffd6fac100df28358a65fc
Steavens <steavens2010@aol.com>
2024-09-22 14:40:30 +0400

    Refactorisation du projet

--- Login/src/main/java/mcci/businessschool/bts/sio/slam/pharmagest/login/LoginApplication.java
+++ Login/src/main/java/mcci/businessschool/bts/sio/slam/pharmagest/login/LoginApplication.java
@@ -1,4 +1,4 @@
-package tuto.login;
+package mcci.businessschool.bts.sio.slam.pharmagest.login;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
@@ -8,9 +8,11 @@ import javafx.stage.Stage;
 import java.io.IOException;
 
 public class LoginApplication extends Application {
+    private final static String MAIN_PATH = "/mcci/businessschool/bts/sio/slam/pharmagest";
+
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(LoginApplication.class.getResource("Login.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(LoginApplication.class.getResource(MAIN_PATH + "/Login.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 520, 400);
         stage.setTitle("Login");
         stage.setScene(scene);
