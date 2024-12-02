ae7986531464031410295d87153d6d2a42e20bc0
Ineees24 <ineeszdm@gmail.com>
2024-12-02 15:56:36 +0400

    Ajout interface Medicament et CrudMedicament, d'autres modifications sont a faire

--- Pharma_gest/Pharma_gest/src/main/java/mcci/businessschool/bts/sio/slam/pharmagest/maintenance/controleur/MaintenanceControleur.java
+++ Pharma_gest/Pharma_gest/src/main/java/mcci/businessschool/bts/sio/slam/pharmagest/maintenance/controleur/MaintenanceControleur.java
@@ -3,9 +3,11 @@ package mcci.businessschool.bts.sio.slam.pharmagest.maintenance.controleur;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.stage.Stage;
+import org.w3c.dom.Node;
 
 import java.io.IOException;
 
@@ -15,6 +17,8 @@ public class MaintenanceControleur {
     private Button retourDashboard;
     @FXML
     private Button userButton;
+    @FXML
+    private Button medicamentButton;
 
     @FXML
     public void retourDashboardOnAction(ActionEvent e) throws IOException {
@@ -38,4 +42,23 @@ public class MaintenanceControleur {
         stage.setScene(nouvelleScene);
     }
 
+
+
+    @FXML
+    public void MedicamentOnAction(ActionEvent e) throws IOException {
+        // Nouvelle scène
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/medicament/Medicament.fxml"));
+        Scene nouvelleScene = new Scene(loader.load());
+
+        // La référence de la scène actuelle
+        Stage stage = (Stage) medicamentButton.getScene().getWindow();
+
+        // Afficher la nouvelle scène
+        stage.setScene(nouvelleScene);
+    }
+
+
 }
+
+
+
